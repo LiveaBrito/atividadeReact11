9eb23026b193353f159509fe7e69d0dc03871d21
Livea Brito <159591382+LiveaBrito@users.noreply.github.com>
2024-10-20 02:23:16 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Texto from './components/Texto'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Texto/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Texto.jsx
@@ -0,0 +1,7 @@
+function Texto(){
+
+    return <h1>Olá Mundo!</h1>
+
+}
+
+export default Texto
